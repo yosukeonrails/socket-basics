cfd4f2978a3b774b16cb5e8bf3858e4d22c505b4
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-05-19 02:28:13 -0700

    add front end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -10,4 +10,17 @@
         console.log(message.text);
    });
    
-   
+   // handles submiting of new message 
+       var $form = jQuery('#message-form');
+
+       var $message= $form.find('input[name=message]')
+
+     $form.on('submit' ,  function (event){
+          event.preventDefault();
+
+          socket.emit('message' , {
+          	text: $message.val()
+          })
+
+          $message.val('');
+     });
